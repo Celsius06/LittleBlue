4beca827e2021ce109c9010eb62781c9d2696a65
Celsius06 <minhthuan24062004@gmail.com>
2024-09-19 22:25:03 +0700

    object interaction (phase 1)

--- src/object/SuperObject.java
+++ src/object/SuperObject.java
@@ -2,6 +2,7 @@
 package object;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 import main.GamePanel;
@@ -11,6 +12,9 @@ public class SuperObject {
 	public String name;
 	public boolean collision = false;
 	public int worldX, worldY;
+	public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
+	public int solidAreaDefaultX = 0;
+	public int solidAreaDefaultY = 0;
 	
 	// Methods
 	public void draw(Graphics2D g2, GamePanel gp) {
