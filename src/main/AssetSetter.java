f7292cf2ab1bfa20c920b02f4db97331c1997740
Celsius06 <minhthuan24062004@gmail.com>
2024-09-26 23:28:53 +0700

    tiles & map updates, fix some bugs and test some new things

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -29,7 +29,7 @@ public class AssetSetter {
 		
 		gp.obj[3] = new OBJ_Door(gp);
 		gp.obj[3].worldX = 10 * gp.tileSize;
-		gp.obj[3].worldY = 11 * gp.tileSize;
+		gp.obj[3].worldY = 12 * gp.tileSize;
 		
 		gp.obj[4] = new OBJ_Door(gp);
 		gp.obj[4].worldX = 8 * gp.tileSize;
@@ -37,14 +37,28 @@ public class AssetSetter {
 		
 		gp.obj[5] = new OBJ_Door(gp);
 		gp.obj[5].worldX = 12 * gp.tileSize;
-		gp.obj[5].worldY = 22 * gp.tileSize;
+		gp.obj[5].worldY = 23 * gp.tileSize;
 		
 		gp.obj[6] = new OBJ_Chest(gp);
 		gp.obj[6].worldX = 10 * gp.tileSize;
-		gp.obj[6].worldY = 7 * gp.tileSize;
+		gp.obj[6].worldY = 8 * gp.tileSize;
 		
 		gp.obj[7] = new OBJ_Boots(gp);
 		gp.obj[7].worldX = 37 * gp.tileSize;
 		gp.obj[7].worldY = 42 * gp.tileSize;
+		
+		// For debugging
+		gp.obj[8] = new OBJ_Key(gp);
+		gp.obj[8].worldX = 24 * gp.tileSize;
+		gp.obj[8].worldY = 21 * gp.tileSize;
+		
+		gp.obj[9] = new OBJ_Key(gp);
+		gp.obj[9].worldX = 25 * gp.tileSize;
+		gp.obj[9].worldY = 21 * gp.tileSize;
+		
+		gp.obj[10] = new OBJ_Key(gp);
+		gp.obj[10].worldX = 26 * gp.tileSize;
+		gp.obj[10].worldY = 21 * gp.tileSize;
+
 	}
 }
