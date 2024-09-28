37bd063a36ce0fc17b88a8c42bae668782a37250
Celsius06 <minhthuan24062004@gmail.com>
2024-09-28 23:38:30 +0700

    Implement NPC (Phase 1)

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -3,8 +3,12 @@ package entity;
 
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import main.GamePanel;
+import main.UtilityTool;
 
 public class Entity {
 	GamePanel gp;
@@ -21,5 +25,18 @@ public class Entity {
 	
 	public Entity(GamePanel gp) {
 		this.gp = gp;
+	}
+	
+	// Methods
+	public BufferedImage setup(String imagePath) {
+		UtilityTool uTool = new UtilityTool();
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(getClass().getResourceAsStream(imagePath + ".png"));
+			image = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return image;
 	}
 }
